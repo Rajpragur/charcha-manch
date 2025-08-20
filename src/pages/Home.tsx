ee1352b8cf1a026f42e102637f8b125e66778254
Rahul kumar <141111854+Kumar-rahul18@users.noreply.github.com>
2025-08-20 21:20:09 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -341,17 +341,40 @@ const Home: React.FC<HomeProps> = () => {
                 </div>
               )}
             </div>
-            <div className="text-center">
-              <p className="text-lg sm:text-xl max-w-4xl mx-auto px-4 mt-4 text-slate-200">
-                {!isEnglish ? "जाने - उम्मीदवारो की सम्पत्ति, आप्राधिक मामले और संसद में भागीदारी" : "Know - The candidates' assets, criminal cases and participation in Parliament"}
-              </p>
-              <p className="text-lg sm:text-xl max-w-4xl mx-auto px-4 my-2 text-slate-200">
-                {!isEnglish ? "रखे - वर्तमान और पूर्व उम्मीदवारो पर अपनी राय" : "Share - Your views on current and past candidates"}
-              </p>
-              <p className="text-lg sm:text-xl max-w-4xl mx-auto px-4 my-2 text-slate-200">
-                {!isEnglish ? "करे - जनसंवाद, सवाल-जवाब और जवाबदेही तय" : "Do – Public dialogue, questions and answers and fix accountability"}
-              </p>
-            </div>
+            <div className="text-center space-y-4 mt-6">
+  <p className="max-w-4xl mx-auto px-4">
+    <span className="block text-2xl sm:text-3xl font-bold text-slate-50">
+      {!isEnglish ? "जाने" : "Know"}
+    </span>
+    <span className="block text-sm sm:text-base text-slate-200">
+      {!isEnglish
+        ? "उम्मीदवारों की सम्पत्ति, आपराधिक मामले और संसद में भागीदारी"
+        : "The candidates' assets, criminal cases and participation in Parliament"}
+    </span>
+  </p>
+
+  <p className="max-w-4xl mx-auto px-4">
+    <span className="block text-2xl sm:text-3xl font-bold text-slate-50">
+      {!isEnglish ? "रखें" : "Share"}
+    </span>
+    <span className="block text-sm sm:text-base text-slate-200">
+      {!isEnglish
+        ? "वर्तमान और पूर्व उम्मीदवारों पर अपनी राय"
+        : "Your views on current and past candidates"}
+    </span>
+  </p>
+
+  <p className="max-w-4xl mx-auto px-4">
+    <span className="block text-2xl sm:text-3xl font-bold text-slate-50">
+      {!isEnglish ? "करें" : "Do"}
+    </span>
+    <span className="block text-sm sm:text-base text-slate-200">
+      {!isEnglish
+        ? "जनसंवाद, सवाल-जवाब और जवाबदेही तय"
+        : "Public dialogue, questions and answers, and fix accountability"}
+    </span>
+  </p>
+</div>
           </div>
         </div>
       </div>
